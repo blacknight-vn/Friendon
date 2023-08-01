ea2bec81b5d0a2b9de1d1905e1ad828cd0cbed7f
Dương Chí Thành <duongchithanh@MacBook-Pro-cua-Duong.local>
2023-08-01 13:45:26 +0700

    second

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
+import {useEffect} from 'react';
 
 function App() {
+
+    useEffect(() => {
+        axios.get('https://bakcend-f3h5.onrender.com/')
+        // axios.get('http://localhost:8080/')
+        .then((response) => {
+            console.log(response.data.message)
+        })
+    }, [])
+
     return (
         <h1>Hello From React</h1>
     )
